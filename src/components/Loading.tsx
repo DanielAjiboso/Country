27e8f34091b211fa47e11a9cd57195eb2ad8d645
DanielAjiboso <danielajiboso@gmail>
2024-06-16 23:14:12 +0200

    next commit

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import { CSSProperties } from "react";
+import ClipLoader from "react-spinners/ClipLoader";
+
+const override: CSSProperties = {
+  display: "block",
+  margin: "0 auto",
+  borderColor: "blue",
+};
 
 const Loading: React.FC = () => {
-  return <div></div>;
+  return (
+    <div>
+      <ClipLoader
+        color="blue"
+        cssOverride={override}
+        size={600}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+      />
+    </div>
+  );
 };
 
 export default Loading;
